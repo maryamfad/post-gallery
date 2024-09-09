7d697e1ef4306d86d67065c4257b399a5dcf2f6e
Maryam Fadaee <maryamfadaee@gmail.com>
2024-09-09 10:47:01 -0400

    Wrap the app with ApolloProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ApolloProvider } from "@apollo/client";
+import client from "./apollo/client.ts";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	</StrictMode>
+);
